5a1ca12b9ed915d69da6e6bf5b862070f62c93a3
jfernandesdev <jfernandes.dev@gmail.com>
2022-07-31 13:57:49 -0300

    STYLE: intro section of home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,59 @@
-import { ContainerHome } from './styles'
+import {
+  ContainerHome,
+  Title,
+  Subtitle,
+  ListFeatures,
+  CycleWithIcon,
+} from './styles'
 
+import mockupHome from '../../assets/banner-intro.png'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 export function Home() {
   return (
     <ContainerHome>
-      <h1>Home</h1>
+      <div>
+        <Title>
+          Encontre o café perfeito <br />
+          para qualquer hora do dia
+        </Title>
+        <Subtitle>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </Subtitle>
+
+        <ListFeatures>
+          <li>
+            <CycleWithIcon bgColor="orange">
+              <ShoppingCart size={16} weight="fill" />
+            </CycleWithIcon>
+            Compra simples e segura
+          </li>
+          <li>
+            {' '}
+            <CycleWithIcon bgColor="gray">
+              <Package size={16} weight="fill" />
+            </CycleWithIcon>
+            Embalagem mantém o café intacto
+          </li>
+          <li>
+            <CycleWithIcon bgColor="yellow">
+              <Timer size={16} weight="fill" />
+            </CycleWithIcon>
+            Entrega rápida e rastreada
+          </li>
+          <li>
+            <CycleWithIcon bgColor="purple">
+              <Coffee size={16} weight="fill" />
+            </CycleWithIcon>
+            O café chega fresquinho até você
+          </li>
+        </ListFeatures>
+      </div>
+
+      <img
+        src={mockupHome}
+        alt="Um copo branco de café com tampa preta, sobre fundo amarelo com as folhas de uma árvore de café e vários cafés em grãos e moído"
+      />
     </ContainerHome>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,79 @@
 import styled from 'styled-components'
 
-export const ContainerHome = styled.div``
+import backgroundHomeIntro from '../../assets/background-home-intro.svg'
+
+export const ContainerHome = styled.div`
+  background: url(${backgroundHomeIntro});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: calc(100vh - 6.5rem);
+
+  display: grid;
+  grid-template-columns: 60% 40%;
+  align-items: center;
+  justify-content: center;
+
+  padding: 5.75rem 10rem;
+
+  > img {
+    width: 476px;
+    height: 360px;
+  }
+`
+
+export const Title = styled.h1`
+  font-family: 'Baloo 2', sans-serif;
+  font-weight: 800;
+  font-size: 3rem;
+  line-height: 3.8rem;
+  color: ${(props) => props.theme['base-title']};
+  margin-bottom: 1rem;
+`
+
+export const Subtitle = styled.h4`
+  font-size: 1.19rem;
+  line-height: 1.62rem;
+  color: ${(props) => props.theme['base-subtitle']};
+  font-weight: 400;
+  font-stretch: 100;
+  white-space: nowrap;
+`
+
+export const ListFeatures = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.25rem 0;
+  margin-top: 5rem;
+  list-style: none;
+
+  li {
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    line-height: 1.32rem;
+  }
+`
+const CYCLE_COLORS = {
+  orange: 'yellow-dark',
+  yellow: 'yellow',
+  gray: 'base-text',
+  purple: 'purple',
+} as const
+
+interface CycleWithIconProps {
+  bgColor: keyof typeof CYCLE_COLORS
+}
+
+export const CycleWithIcon = styled.span<CycleWithIconProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+
+  background: ${(props) => props.theme[CYCLE_COLORS[props.bgColor]]};
+  color: ${(props) => props.theme.background};
+`
